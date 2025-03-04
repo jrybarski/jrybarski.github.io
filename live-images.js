85b1762e3aa313bea42d3ca71ffa4f61aa5dada3
Jakub Rybarski <jakubrybarski21@gmail.com>
2025-03-04 16:07:50 +0100

    zmiany

--- live-images.js
+++ live-images.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.querySelectorAll(".image-live, .background, .log"); // Dodano .log
+    // Obserwacja elementów, które mają pojawiać się przy przewijaniu
+    const elements = document.querySelectorAll(".image-live, .background, .log");
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
@@ -11,4 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }, { threshold: 0.3 }); // 30% widoczności aktywuje animację
 
     elements.forEach(element => observer.observe(element));
+
+    // Efekt pojawiania się logo na środku i jego zniknięcia
+    const logoContainer = document.querySelector(".logo-container");
+    if (logoContainer) {
+        setTimeout(() => {
+            logoContainer.classList.add("hide");
+        }, 500); // Logo znika po 2 sekundach
+    }
 });
